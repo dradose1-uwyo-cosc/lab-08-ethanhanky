49df28a808e4ffc5e1000d8c17fb96ca8fffe236
ethanhanky <efhankel@icloud.com>
2024-11-05 18:25:18 -0700

    Final

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# [Name]
-## [Assignment #]
-## [Submission date: MM/DD/YY]
+# [Ethan Hankel]
+## [Lab 8]
+## [Submission date: 11/05/24]
 ## Worked with/sources 
-* example
-* example
-* example
-* example
+* I used the functions and while loop powerpoints
+* Then when i had mistakes in my code or it wasn't working correctly than i used Chat GPT
 ## Project Quirks/ Things that don't work
-* example
-* example
-* example
+* I had a bunch of errors due to extra spaces in the functions
+* Didnt really effect me much it was more annoying
+* Had my int check function spelled wrong which confused me
+* Had lots of trouble getting it to sync to github
--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,12 +1,12 @@
-# Your Name Here
+# Ethan Hankel
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section:
+# Submission Date: 11/05/24
+# Lab 8
+# Lab Section: 02
 # Sources, people worked with, help given to:
-# your
-# comments
-# here
+# I used the functions and while loop powerpoints
+# Then when i had mistakes in my code or it wasn't working correctly than i used Chat GPT
+# Had a lot of problems with having an extra space in my functions
 
 
 # Write a function that will properly check strings to see if they are an int or float, and convert them if so
@@ -14,6 +14,19 @@
 # Other wise return the converted int or float 
 # Floats should only have one decimal point in them 
 
+def int_check(value):
+    try:
+        int_value = int(value)
+        return int_value
+    except ValueError:
+        try:
+            float_value = float(value)
+            if value.count(".") == 1:
+                return float_value
+            else:
+                return False
+        except ValueError:
+            return False
 
 print("*" * 75)
 
@@ -38,8 +51,19 @@ print("*" * 75)
 # Remember all inputs are strings, but the function needs ints or floats
 # Call your function and print the resulting list
 
-print("*" * 75)
+def slope_intercept(m, b, low_bound, up_bound):
+    if not isinstance(low_bound, int) or not isinstance(up_bound, int):
+        return False
+    if low_bound > up_bound:
+        return False
+    
+    y_values = []
+    for x in range(low_bound, up_bound + 1):
+        y = m * x + b
+        y_values.append(y)
+    return y_values
 
+print("*" * 75)
 
 # Write a function to solve the quadratic formula
 # https://en.wikipedia.org/wiki/Quadratic_formula
@@ -48,3 +72,63 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+
+
+import math
+
+
+def safe_sqrt(value):
+    if value < 0:
+        return None
+    return math.sqrt(value)
+
+def quad_form(a, b, c):
+    discrim = b**2 - 4*a*c
+    sqrt_discrim = safe_sqrt(discrim)
+    if sqrt_discrim is None:
+        return None
+
+    first_root = (-b + sqrt_discrim) / (2 * a)
+    second_root = (-b - sqrt_discrim) / (2 * a)
+    return first_root, second_root
+
+
+while True:
+    m = input("Enter Slope (m) or exit to quit: ")
+    if m.lower() == 'exit': 
+        break
+    b = input("Enter intercept (b): ")
+    low_bound = input("Enter Lower Bound for x: ")
+    up_bound = input("Enter upper Bound for x: ")
+
+    m = int_check(m)
+    b = int_check(b)
+    low_bound = int_check(low_bound)
+    up_bound = int_check(up_bound)
+
+    if isinstance(m, (int, float)) and isinstance(b, (int, float)) \
+        and isinstance(low_bound, int) and isinstance(up_bound, int):
+        y_values = slope_intercept(m, b, low_bound, up_bound)
+        print("y values:", y_values)
+    else:
+        print("Please enter actual numbers!")
+    
+    while True:
+        a = input("Enter coefficient a: ")
+        if a.lower() == 'exit':
+            break
+        b = input("Enter coefficient b: ")
+        c = input("Enter coefficient c: ")
+
+        a = int_check(a)
+        b = int_check(b)
+        c = int_check(c)
+
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)):
+            roots = quad_form(a, b, c)
+            print("Roots", roots)
+        else:
+            print("Please enter actual number!")
+
+
+
